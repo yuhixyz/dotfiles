b240d3fa210f62a1106177b3fd4339cce2f41be0
yuhixyz <2539012056@qq.com>
2022-05-31 00:20:01 +0800

    update yabai && sketchybar

---
+++ yabai/.config/sketchybar/plugins/space.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+if [ "$SELECTED" = "true" ]; then
+  args+=(--set $NAME icon.background.y_offset=-12)
+else
+  args+=(--set $NAME icon.background.y_offset=-20)
+fi
+
+args+=(icon.highlight=$SELECTED)
+
+sketchybar --animate tanh 20 "${args[@]}"
