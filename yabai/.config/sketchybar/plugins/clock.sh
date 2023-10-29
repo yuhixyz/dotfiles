4a450a9ed992af8ad179225fbde0022b474b7d37
yuhixyz <yuhixyz@outlook.com>
2023-10-29 19:12:13 +0800

    yabai manage IDEA main window

--- yabai/.config/sketchybar/plugins/clock.sh
+++ yabai/.config/sketchybar/plugins/clock.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env sh
 
-sketchybar --set $NAME label="$(date +'%I:%M %p')"
+sketchybar --set $NAME label="$(date '+%H:%M')"
+# sketchybar --set $NAME label="$(date '+%I:%M %p')"
+# sketchybar --set $NAME label="$(date '+%I:%M %p') -- $(TZ=Europe/London date '+%I:%M %p')"
 
